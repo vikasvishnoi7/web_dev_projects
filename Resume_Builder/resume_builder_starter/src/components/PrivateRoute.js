9af99e5bf4bf55c744906e239e1767e1fc66450d
vikas vishnoi <vikasvishnoi2529@gmail.com>
2022-02-10 20:59:34 +0530

    update PrivateRoute

--- Resume_Builder/resume_builder_starter/src/components/PrivateRoute.js
+++ Resume_Builder/resume_builder_starter/src/components/PrivateRoute.js
@@ -20,32 +20,3 @@ function mapStateToProps(state) {
 export default connect(mapStateToProps)(PrivateRoute)
 
 
-
-
-
-
-/**
- * 
- * import React from 'react'
-import { Route, Redirect } from 'react-router-dom';
-import { isLoaded, isEmpty } from 'react-redux-firebase'
-import { connect } from 'react-redux';
-
-function PrivateRoute({ auth, component: Component, ...remainingprops }) {
-
-    return (
-        <Route {...remainingprops} render={(props) => (
-            isLoaded(auth) && !isEmpty(auth) ?
-                <Component {...props} /> : <Redirect to='/' />
-        )} />
-    )
-}
-
-const mapStateToProps = (state) => {
-    return {
-        auth: state.firebase.auth
-    }
-}
-
-export default connect(mapStateToProps)(PrivateRoute)
- */
