64ce705d8204a9d3538e1b17310a60ff04324da2
vikas vishnoi <vikasvishnoi2529@gmail.com>
2022-03-02 19:45:22 +0530

    update

--- automateWebScraping/youTube/youTubeAutoScrapAwait.js
+++ automateWebScraping/youTube/youTubeAutoScrapAwait.js
@@ -2,11 +2,6 @@
 // b. Total No of videos : 792
 // c. actual No of videos :783
 // d. Total length of playlist : 12 hours, 9 minutes, 12 seconds
-// At 1.25x : 9 hours, 43 minutes, 21 seconds
-// At 1.50x : 8 hours, 6 minutes, 8 seconds
-// At 1.75x : 6 hours, 56 minutes, 41 seconds
-// At 2.00x : 6 hours, 4 minutes, 36 seconds
-// Average length of video : 29 minutes, 10 seconds
 
 // e. console.table=>  of video number,name,time
 
@@ -27,7 +22,7 @@ let page;
         args: ["--start-maximized"],
     })
     page = await browser.newPage();
-    await page.goto("https://www.youtube.com/playlist?list=PLzkuLC6Yvumv_Rd5apfPRWEcjf9b1JRnq");
+    await page.goto("https://www.youtube.com/playlist?list=PLRBp0Fe2GpglDkRdEd_BhnSkHo8FgPmzs");
     await page.waitForSelector('h1[id="title"]');
     // first element get by $
     //selector h1[id="title"] -> use -> get playList name
